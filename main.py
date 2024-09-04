de2b6c89a06e2a0fa1c30cb6f8b242cf19d587e7
Bart <bartdb2001@gmail.com>
2024-09-04 11:56:19 +0200

    set up cogs

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import nextcord
-from nextcord import Interaction
-from nextcord.utils import get
-from nextcord.ext import commands, tasks
-from nextcord.ext import application_checks
-from apikey import *
+from nextcord.ext.commands import Bot
+from variablesfile import *
 
 intents = nextcord.Intents.all()
 intents.members = True
 
-client = commands.Bot(command_prefix='&', intents=nextcord.Intents.all())
 
-@client.event
-async def on_ready():
-    print("Bot is ready to do useful shit!\n")
 
-client.run(BOTTOKEN)
+
+bot = Bot()
+
+bot.load_extension("cogs.ping") #the file is in the cogs folder
+
+bot.run(BotToken)
